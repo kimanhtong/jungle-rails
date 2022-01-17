e1ec1d92dbb5ad7ce5d32309fa92b8c4f0c9d6ec
Anh Tong <tongkimanh@yahoo.com>
2022-01-17 20:43:17 +0000

    To complete the links to pages for Sign In and Sign Out buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,6 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     @current_username ||= @current_user.first_name + " " + @current_user.last_name if session[:user_id]
-    puts @current_username
   end
   helper_method :current_user
 
