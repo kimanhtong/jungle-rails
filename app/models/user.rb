40e8f6d239f6a6db7d2efef518fca90200fd064b
Anh Tong <tongkimanh@yahoo.com>
2022-01-28 20:06:52 +0000

    To update user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  def self.authenticate_with_credentials(e, p)
+    # self.find_by_email_and_password_digest([e], [p])
+    self.find_by(email: [e]).try(:authenticate, [p]) 
+  end
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false}
   validates :password_confirmation, presence: true, length: 2..20
+
+  
+
 end
